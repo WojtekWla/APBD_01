04b765346187f1a8546f2878322664227a910270
s27889 <s27889@pjwstk.edu.pl>
2024-03-18 12:52:31 +0100

    Modification 2

--- APBD_01/Program.cs
+++ APBD_01/Program.cs
@@ -4,7 +4,11 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int num1, num2;
+        num1 = Convert.ToInt32(Console.ReadLine());
+        num2 = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine(num1 + num2);
     }
 }
 
