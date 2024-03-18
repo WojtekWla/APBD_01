9f87dc53e30e621e9da3b7ce072eb839f153448a
s27889 <s27889@pjwstk.edu.pl>
2024-03-18 13:21:31 +0100

    Added average function

--- APBD_01/Program.cs
+++ APBD_01/Program.cs
@@ -4,12 +4,31 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        int num1, num2;
-        num1 = Convert.ToInt32(Console.ReadLine());
-        num2 = Convert.ToInt32(Console.ReadLine());
+        //Task 1
+        // int num1, num2;
+        // num1 = Convert.ToInt32(Console.ReadLine());
+        // num2 = Convert.ToInt32(Console.ReadLine());
+        //
+        // Console.WriteLine(num1 + num2);
+        // Console.WriteLine(num1 - num2);
         
-        Console.WriteLine(num1 + num2);
-        Console.WriteLine(num1 - num2);
+        //Task 2
+        int[] nums = new int[] {1,2,4,4,5};
+        Console.WriteLine(average(nums));
+        
+
     }
+
+    public static float average(int[] nums)
+    {
+        float result = 0f;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            result += nums[i];
+        }
+
+        return result / nums.Length;
+    }
+    
 }
 
